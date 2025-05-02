7ba05f12ad95252622ee87db53dbae537ef579c8
Riley1101 <arkardev00@gmail.com>
2025-05-02 13:46:03 -0400

    wsm with effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "xterm/css/xterm.css";
+import wasm from "./pkg";
+import { useEffect } from "react";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    // @ts-expect-error wasm types
+    wasm.__wbindgen_start();
+    return () => {
+      // @ts-expect-error wasm types
+      wasm.__wbindgen_free();
+    };
+  }, []);
+
   return (
     <div className="relative w-full h-dvh overflow-hidden">
       <div id="terminal" className="w-full h-full absolute top-0 left-0" />
     </div>
   );
 }
-
-export default App;
