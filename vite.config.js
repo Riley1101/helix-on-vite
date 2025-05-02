486cff9b0cc5241085eb70fe50ee4de168685272
Riley1101 <arkardev00@gmail.com>
2025-05-02 13:28:39 -0400

    top level await

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), tailwindcss(), wasm()],
+  plugins: [viteReact(), tailwindcss(), wasm(), topLevelAwait()],
   test: {
     globals: true,
     environment: "jsdom",
